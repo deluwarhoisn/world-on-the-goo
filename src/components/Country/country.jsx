bd98e7a730890ffe32c934d8737ac22446710856
deluwarhoisn <babuhossen301@gmail.com>
2025-09-18 23:30:41 +0600

    country

---
+++ src/components/Country/country.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Country = ({ Country }) => {
+    console.log(Country)
+    return (
+        <div>
+            {/* <img src={Country.flags.flags.png} alt={Country.flags.flags.alt} /> */}
+            <h3>Name: {Country}</h3>
+        </div>
+    );
+};
+
+export default Country;
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,16 +1,18 @@
 import React, { use } from 'react';
-import County from './Country/County';
+import Country from '../Country/Country';
+
 
 const Countries = ({countriesPromise}) => {
+
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
     
     return (
         <div>
             <h1>In the countries: {countries.length}</h1>
-            {
-               
-            }
+          {
+            countries.map(country => <Country country={country}></Country>)
+          }
         </div>
     );
 };
