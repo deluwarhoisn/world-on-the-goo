0f457c4bf961bbd39bf68d86f823fb5dcb1eb086
deluwarhoisn <babuhossen301@gmail.com>
2025-09-18 22:53:02 +0600

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import Countries from './components/countries/countries'
 import './App.css'
+import { Suspense } from 'react'
+
+const countriesPromise =fetch('https://openapi.programming-hero.com/api/all')
+.then(res => res.json())
+
 
 function App() {
 
 
   return (
     <>
-    
 
-      <Countries></Countries>
+  <Suspense fallback={<p>Nadir vai loading...</p>}>
+    <Countries countriesPromise={countriesPromise}></Countries>
+  </Suspense>
+      
     </>
   )
 }
---
+++ src/components/countries/Country/County.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const County = () => {
+    return (
+        <div>
+            <h3>Name: </h3>
+        </div>
+    );
+};
+
+export default County;
